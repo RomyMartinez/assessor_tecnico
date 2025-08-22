0c67b8d76856269060eed9cf5ccbf24eb3096286
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-21 23:37:51 -0300

    feat(theme): add theme tokens

---
+++ src/theme/color.ts
@@ -0,0 +1,35 @@
+export const colors = {
+  primary: {
+    light: '#EE7412',
+    dark: '#944607',
+    hover: '#D78F38',
+  },
+  secondary: {
+    light: '#3A91AF',
+    dark: '#1B5E75',
+    hover: '#05b8a1',
+  },
+  tertiary: {
+    light: '#A6C04F',
+    dark: '#6b7f27',
+    hover: '#b5cf55',
+  },
+  background: {
+    light: '#F8F4E6',
+    dark: '#333333',
+  },
+  system: {
+    inactive: '#A7A5A5',
+    error: '#EB5757',
+    success: '#27AE60',
+    link: '#3390b7',
+    alert: '#FFC259',
+  },
+  neutral: {
+    white: '#FFFFFF',
+    light_gray: '#BFBFBF',
+    medium_gray: '#a7a5a5',
+    dark_gray: '#666666',
+    dark: '#333333',
+  },
+};
---
+++ src/theme/fontFamily.ts
@@ -0,0 +1,8 @@
+export const fontFamily = {
+  light: 'Roboto_300Light',
+  regular: 'Roboto_400Regular',
+  medium: 'Roboto_500Medium',
+  semiBold: 'Roboto_600SemiBold',
+  bold: 'Roboto_700Bold',
+  extraBold: 'Roboto_800ExtraBold',
+};

4227b5182aee5b465ef2cd286e81959df858c027
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-25 01:07:37 -0300

    feat: Implementing Request Tab

--- src/app/(tabs)/requests.tsx
+++ src/app/(tabs)/requests.tsx
@@ -1,9 +1,28 @@
 import { View, Text } from 'react-native';
+import { colors } from '@/theme';
+import { useState } from 'react';
+import { MainSurvey } from '@/components/MainSurvey';
+import { SurveyHeader } from '@/components/SurveyHeader';
 
 export default function Requests() {
+  const [selectedSurvey, setSelectedSurvey] = useState('Todas');
+
+  function onPress(label: string) {
+    setSelectedSurvey(label);
+  }
+
   return (
-    <View>
-      <Text>Requests Screen</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: colors.background.light,
+        paddingTop: 80,
+        paddingHorizontal: 16,
+        gap: 16,
+      }}
+    >
+      <SurveyHeader surveyType={selectedSurvey} onPress={onPress} />
+      <MainSurvey surveyType={selectedSurvey} />
     </View>
   );
 }

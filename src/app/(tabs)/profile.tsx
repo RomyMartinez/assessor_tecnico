ac5ecce7bd2442120627adcc099a86b60a250ab7
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-25 01:05:43 -0300

    feat: Implementing Profile

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,9 +1,25 @@
 import { View, Text } from 'react-native';
+import { colors } from '@/theme';
+import { ProfilePicture } from '@/components/ProfilePicture';
+import { Settings } from '@/components/Settings';
 
 export default function Profile() {
   return (
-    <View>
-      <Text>Profile Screen</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: colors.background.light,
+        paddingTop: 104,
+        gap: 80,
+      }}
+    >
+      <ProfilePicture
+        data={{
+          name: 'Arthur Gonçalves',
+          src: require('../../../assets/dc49bac5004484664d96aece63f7a26376eadf2c.jpg'),
+        }}
+      />
+      <Settings />
     </View>
   );
 }

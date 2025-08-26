d3d53aef27a7c6b9c8d3d635560eb5c1a27ff268
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-26 00:33:15 -0300

    feat: add scroll functionality to SurveyDescription component and clean up duplicate code

--- src/components/SurveyDescription/index.tsx
+++ src/components/SurveyDescription/index.tsx
@@ -1,10 +1,62 @@
-import { View } from 'react-native';
-import { Survey } from '@/types/survey';
+import { View, Text, ScrollView } from "react-native";
+import { Survey } from "@/types/survey";
+import { styles } from "./styles";
+import { getDay } from "@/util/date";
+import { MaterialIcons } from "@expo/vector-icons";
+import { colors } from "@/theme";
 
 type SurveyDescriptionProps = {
   data: Survey;
 };
 
 export function SurveyDescription({ data }: SurveyDescriptionProps) {
-  return <View></View>;
+  const day = getDay(data.schedule.date);
+  const [year, month, dayNumber] = data.schedule.date.split("-");
+  const [hour, minute] = data.schedule.time.split(":");
+  const isMorning = Number(hour) < 12;
+  const time = isMorning ? "Manhã" : "Tarde";
+
+  return (
+    <ScrollView
+      style={styles.container}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ gap: 16 }}
+    >
+      <View style={[styles.contentContainer, { gap: 8 }]}>
+        <Text style={styles.title}>Visita Agendada</Text>
+        <View style={styles.date}>
+          <View style={{ flexDirection: "row", gap: 2, alignItems: "center" }}>
+            <MaterialIcons
+              name="calendar-month"
+              size={16}
+              color={colors.neutral.dark}
+            />
+            <View style={{ flexDirection: "row" }}>
+              <Text style={styles.textMedium}>
+                {dayNumber}/{month}/{year} -{" "}
+                <Text style={styles.textRegular}>{day}</Text>
+              </Text>
+            </View>
+          </View>
+          <Text style={styles.textMedium}>
+            Turno: <Text style={styles.textRegular}>{time}</Text>
+          </Text>
+        </View>
+      </View>
+      <View style={[styles.contentContainer, { gap: 8 }]}>
+        <View style={{ gap: 8 }}>
+          <Text style={styles.title}>Descrição</Text>
+          <Text style={styles.textDescription}>{data.data.description}</Text>
+        </View>
+      </View>
+      <View style={[styles.contentContainer, { gap: 8 }]}>
+        <Text style={styles.title}>Dados do Beneficiário</Text>
+        <Text style={styles.textBold}>{data.data.name}</Text>
+        <Text style={styles.textRegular}>{data.data.location}</Text>
+        <Text style={styles.textMedium}>
+          Telefone: <Text style={styles.textRegular}>{data.data.phone}</Text>
+        </Text>
+      </View>
+    </ScrollView>
+  );
 }
--- src/components/SurveyDescription/styles.ts
+++ src/components/SurveyDescription/styles.ts
@@ -1,10 +1,47 @@
-import { StyleSheet } from 'react-native';
-import { Title } from 'react-native-paper';
+import { StyleSheet } from "react-native";
+import { colors, fontFamily } from "@/theme";
 
 export const styles = StyleSheet.create({
-  title: {},
-  description: {},
-  benefitedData: {},
-  localization: {},
-  status: {},
+  container: {
+    flex: 1,
+    gap: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontFamily: fontFamily.medium,
+    color: colors.primary.light,
+  },
+  contentContainer: {
+    width: "100%",
+    borderWidth: 2,
+    borderColor: colors.primary.light,
+    padding: 16,
+    borderRadius: 20,
+  },
+  date: {
+    flexDirection: "row",
+    gap: 6,
+    alignItems: "center",
+  },
+  textBold: {
+    fontFamily: fontFamily.bold,
+    fontSize: 20,
+    color: colors.neutral.dark,
+  },
+  textMedium: {
+    fontFamily: fontFamily.medium,
+    fontSize: 16,
+    color: colors.neutral.dark,
+  },
+  textRegular: {
+    fontFamily: fontFamily.regular,
+    fontSize: 16,
+    color: colors.neutral.dark,
+  },
+
+  textDescription: {
+    fontFamily: fontFamily.regular,
+    fontSize: 18,
+    color: colors.neutral.dark,
+  },
 });

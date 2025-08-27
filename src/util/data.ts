656268240d6c8a79e98d2dc79b5ede282ce49ff0
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-26 23:45:34 -0300

    feat(util): update data structure and add new survey examples

--- src/util/data.ts
+++ src/util/data.ts
@@ -1,66 +1,76 @@
-import type { Survey } from '@/types/survey';
+import type { Survey } from "@/types/survey";
 
 export const surveys: Survey[] = [
   {
-    id: '#0001',
-    state: 'active',
-    type: 'Beneficiado',
+    id: "#0001",
+    state: "active",
+    type: "Beneficiado",
     schedule: {
-      date: '2023-09-01',
-      time: '10:00h',
+      date: "2023-09-01",
+      time: "10:00h",
     },
     data: {
-      name: 'Maria de Fátima',
+      name: "Maria de Fátima",
       location:
-        'Av. Atlântica, 1702 - Copacabana, Rio de Janeiro - RJ, 22021-001',
+        "Av. Atlântica, 1702 - Copacabana, Rio de Janeiro - RJ, 22021-001",
       description:
-        'Oi, eu não entendo muito de obra, mas preciso arrumar o quarto aqui de casa, tem alguns buracos na parede e não sei o que é necessário para arrumar.',
-      phone: '22 99999-999',
+        "Oi, eu não entendo muito de obra, mas preciso arrumar o quarto aqui de casa, tem alguns buracos na parede e não sei o que é necessário para arrumar.",
+      phone: "22 99999-999",
       images: [
-        'https://via.placeholder.com/150',
-        'https://via.placeholder.com/150',
+        require("../../assets/benefited_0001/1.png"),
+        require("../../assets/benefited_0001/2.png"),
+        require("../../assets/benefited_0001/3.png"),
+        require("../../assets/benefited_0001/5.png"),
+        require("../../assets/benefited_0001/6.png"),
       ],
     },
   },
   {
-    id: '#0002',
-    state: 'active',
-    type: 'Beneficiado',
+    id: "#0002",
+    state: "active",
+    type: "Beneficiado",
     schedule: {
-      date: '2023-09-01',
-      time: '10:00h',
+      date: "2023-09-01",
+      time: "10:00h",
     },
     data: {
-      name: 'César Ribeiro',
+      name: "César Ribeiro",
       location:
-        'Av. Atlântica, 1702 - Copacabana, Rio de Janeiro - RJ, 22021-001',
+        "Av. Atlântica, 1702 - Copacabana, Rio de Janeiro - RJ, 22021-001",
       description:
-        'Oi, eu não entendo muito de obra, mas preciso arrumar o quarto aqui de casa, tem alguns buracos na parede e não sei o que é necessário para arrumar.',
-      phone: '22 99999-999',
+        "Oi, eu não entendo muito de obra, mas preciso arrumar o quarto aqui de casa, tem alguns buracos na parede e não sei o que é necessário para arrumar.",
+      phone: "22 99999-999",
       images: [
-        'https://via.placeholder.com/150',
-        'https://via.placeholder.com/150',
+        require("../../assets/benefited_0001/1.png"),
+        require("../../assets/benefited_0001/2.png"),
+        require("../../assets/benefited_0001/3.png"),
+        require("../../assets/benefited_0001/4.png"),
+        require("../../assets/benefited_0001/5.png"),
       ],
     },
   },
   {
-    id: '#0003',
-    state: 'active',
-    type: 'Doador',
+    id: "#0003",
+    state: "active",
+    type: "Doador",
     schedule: {
-      date: '2023-09-01',
-      time: '10:00h',
+      date: "2023-09-01",
+      time: "10:00h",
     },
     data: {
-      name: 'Raquel Acioly',
+      name: "Raquel Acioly",
       location:
-        'Av. Atlântica, 1702 - Copacabana, Rio de Janeiro - RJ, 22021-001',
+        "Av. Atlântica, 1702 - Copacabana, Rio de Janeiro - RJ, 22021-001",
       description:
-        'acabei de reformar minha casa e sobraram alguns mateiais: areia, cimento, brita... gostaria de fazer uma doação',
-      phone: '22 99999-999',
+        "acabei de reformar minha casa e sobraram alguns mateiais: areia, cimento, brita... gostaria de fazer uma doação",
+      phone: "22 99999-999",
       images: [
-        'https://via.placeholder.com/150',
-        'https://via.placeholder.com/150',
+        require("../../assets/benefited_0001/1.png"),
+        require("../../assets/benefited_0001/2.png"),
+        require("../../assets/benefited_0001/3.png"),
+        require("../../assets/benefited_0001/4.png"),
+        require("../../assets/benefited_0001/5.png"),
+        require("../../assets/benefited_0001/6.png"),
       ],
     },
   },
